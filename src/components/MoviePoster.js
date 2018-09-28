7b551ee6f36df97a08e1f0f3ca45c5188a27b583
mattc0llier <matt.collier@hotmail.com>
2018-09-28 15:47:35 +0100

    fecth movie from api on click working

--- src/components/MoviePoster.js
+++ src/components/MoviePoster.js
@@ -1,10 +1,33 @@
 import React from "react";
 
 class MoviePoster extends React.Component {
+  constructor() {
+    super();
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    this.props.receiveMovie(this.props.movieObject);
+  }
+
   render() {
     return (
       <div>
-        <h1>Poster</h1>
+        <img
+          onClick={this.handleClick}
+          src={this.props.movieObject.Poster}
+          alt={this.props.movieObject.Title}
+        />
+        <h4>{this.props.movieObject.Title}</h4>
+        <div className="year">
+          <p>{this.props.movieObject.Year}</p>
+          <a
+            href={`https://www.imdb.com/title/${this.props.movieObject.imdbID}`}
+          >
+            IMDB
+          </a>
+        </div>
       </div>
     );
   }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,11 +2,43 @@ import React from "react";
 import MoviePoster from "./MoviePoster.js";
 
 class Movies extends React.Component {
+  constructor() {
+    super();
+
+    this.state = { image: "" };
+
+    this.receiveMovie = this.receiveMovie.bind(this);
+    this.fetchMovie = this.fetchMovie.bind(this);
+  }
+
+  fetchMovie(movieImdbID) {
+    fetch(`http://www.omdbapi.com/?i=${movieImdbID}&apikey=edd66bb`)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(body => {
+        console.log(body);
+      });
+  }
+
+  receiveMovie(movie) {
+    this.setState(
+      {
+        movie: movie
+      },
+      () => this.fetchMovie(this.state.movie.imdbID)
+    );
+  }
+
   render() {
     return (
       <div>
         {this.props.movies.map(movie => (
-          <MoviePoster key={movie.imdbID} movieObject={movie} />
+          <MoviePoster
+            key={movie.imdbID}
+            movieObject={movie}
+            receiveMovie={this.receiveMovie}
+          />
         ))}
       </div>
     );
